fc17837e5b740b10c15f42b48c85a0e5161c3d68
ghila05 <nicolaghi05@gmail.com>
2022-09-28 10:36:58 +0200

    finita ricerca binaria, da sistemare grafica e campi da far vedere all'utente

--- accessodirettofile(2)/Form1.Designer.cs
+++ accessodirettofile(2)/Form1.Designer.cs
@@ -29,46 +29,38 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox1_ingresso = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 21);
+            this.button1.Location = new System.Drawing.Point(182, 113);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 94);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // textBox1_ingresso
             // 
-            this.textBox1_ingresso.Location = new System.Drawing.Point(12, 21);
+            this.textBox1_ingresso.Location = new System.Drawing.Point(129, 41);
+            this.textBox1_ingresso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1_ingresso.Multiline = true;
             this.textBox1_ingresso.Name = "textBox1_ingresso";
-            this.textBox1_ingresso.Size = new System.Drawing.Size(100, 20);
+            this.textBox1_ingresso.Size = new System.Drawing.Size(227, 50);
             this.textBox1_ingresso.TabIndex = 2;
             this.textBox1_ingresso.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 380);
+            this.ClientSize = new System.Drawing.Size(483, 189);
             this.Controls.Add(this.textBox1_ingresso);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -79,7 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1_ingresso;
     }
 }
